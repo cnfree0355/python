057307135ada1da3f29765d627074e4488a0dfd4
cnfree0355 <free5035gg@gmail.com>
2016-07-11 22:41:37 +0800

    新增和删除部分文件

--- url.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import urllib
-import urllib2
-#response = urllib2.urlopen('http://www.soouya.com')
-request = urllib2.Request('http://www.soouya.com')
-response = urllib2.urlopen(request)
-html = response.read()
-print html 
-
