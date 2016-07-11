8458718f7963526ab936e123a108ef5f319b536e
cnfree0355 <free5035gg@gmail.com>
2016-07-11 23:03:15 +0800

    新增目录归纳移动部分文件

---
+++ Web/mvc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from flask import Flask,request,render_template
+app = Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/login',methods=['GET'])
+def login_form():
+    return render_template('form.html')
+
+@app.route('/login',methods=['post'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    if username == 'admin' and password == 'admin123':
+        return render_template('login-ok.html',username = username)
+
+
+if __name__ == '__main__':
+    app.run('10.9.0.110',5000,debug=True)
+
+
---
+++ function/function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+def choose(x,y):
+  if x >y:
+    print 'x is bigger'
+  else:
+   print 'y is bigger'
+x = int(raw_input('type the x here'))
+
+y  = int(raw_input('type the y here'))
+choose(x,y)
+
+a = '*'
+print a*15
+#for  the future
+def tt(hh):
+    pass
+def test(user):
+  print 'hello , i love %s ' % (user)
+
+test('liu')
